c25ee96c34ad7a5b87d92af17e20923097d97143
Michaeljcohen <mcohen3@gmail.com>
2016-07-19 18:05:24 -0700

    Added simple table storage library which can be used to read / write image data

---
+++ TableStorageClient/TableStorageClient/TableEntity.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+public class CustomerEntity : TableEntity
+{
+    public CustomerEntity(string lastName, string firstName)
+    {
+        this.PartitionKey = lastName;
+        this.RowKey = firstName;
+    }
+
+    public CustomerEntity() { }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+}
---
+++ TableStorageClient/TableStorageClient/TableStorageClient.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.Azure; // Namespace for CloudConfigurationManager 
+using Microsoft.WindowsAzure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.WindowsAzure.Storage.Table; // Namespace for Table storage types
+using System;
+
+namespace TableStorageClient
+{
+    public class TableStorageClient
+    {
+        public static void Main(string[] args)
+        {
+            // Parse the connection string and return a reference to the storage account.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            string tableName = "people";
+            CreateTable(tableName);
+
+            // Create a new customer entity.
+            CustomerEntity customer1 = new CustomerEntity("Harp", "Walter");
+            customer1.Email = "Walter@contoso.com";
+            customer1.PhoneNumber = "425-555-0101";
+
+            InsertRecord(tableName, customer1);
+        }
+
+        public static void CreateTable(string tableReferenceName)
+        {
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            // Retrieve a reference to the table.
+            CloudTable table = tableClient.GetTableReference(tableReferenceName);
+            // Create the table if it doesn't exist.
+            table.CreateIfNotExists();
+        }
+
+        public static void InsertRecord(string tableReferenceName, TableEntity record)
+        {
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+               CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Create the CloudTable object that represents the "people" table.
+            CloudTable table = tableClient.GetTableReference(tableReferenceName);
+
+            // Create the TableOperation object that inserts the customer entity.
+            TableOperation insertOperation = TableOperation.Insert(record);
+
+            // Execute the insert operation.
+            table.Execute(insertOperation);
+        }
+
+        public static void GetRecordsInPartition(string tableReferenceName, string partitionName)
+        {
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Create the CloudTable object that represents the "people" table.
+            CloudTable table = tableClient.GetTableReference(tableReferenceName);
+
+            // Construct the query operation for all customer entities where PartitionKey="Smith".
+            TableQuery<CustomerEntity> query = new TableQuery<CustomerEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, partitionName));
+
+            // Print the fields for each customer.
+            foreach (CustomerEntity entity in table.ExecuteQuery(query))
+            {
+                Console.WriteLine("{0}, {1}\t{2}\t{3}", entity.PartitionKey, entity.RowKey,
+                    entity.Email, entity.PhoneNumber);
+            }
+        }
+
+        public static void GetRecordsByPartitionAndSubkey(string tableReferenceName, string partitionName, string subKeyName)
+        {
+            // Retrieve the storage account from the connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+
+            // Create the table client.
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Create the CloudTable object that represents the "people" table.
+            CloudTable table = tableClient.GetTableReference(tableReferenceName);
+
+            // Create the table query.
+            TableQuery<CustomerEntity> rangeQuery = new TableQuery<CustomerEntity>().Where(
+                TableQuery.CombineFilters(
+                    TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, partitionName),
+                    TableOperators.And,
+                    TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, subKeyName)));
+
+            // Loop through the results, displaying information about the entity.
+            foreach (CustomerEntity entity in table.ExecuteQuery(rangeQuery))
+            {
+                Console.WriteLine("{0}, {1}\t{2}\t{3}", entity.PartitionKey, entity.RowKey,
+                    entity.Email, entity.PhoneNumber);
+            }
+        }
+    }
+}
