cccc0d7813cf9156e4f18089638160aec477b633
Chen LiangYu <commbigo@gmail.com>
2016-07-25 15:40:59 -0700

    add Report To Excel, Get/Export APIs

---
+++ ReportingWeb/ReportingWeb/Controllers/ExportController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ReportingWeb.ExportFiles;
+using TableStorageClient;
+using System.IO;
+
+namespace ReportingWeb.Controllers
+{
+    public class ExportController : Controller
+    {
+        private static string StorageConnectionString = "DefaultEndpointsProtocol=https;AccountName=cohenhackathon;AccountKey=2EIamWgLyH38CBBqJRAULJTvNOHlqRZiaeb/x16c7q23YJJtPqVI+vTmsyCj6+eUfeOajX7c6cjUkGHB0JrlMg==";
+
+        private TableStorageClient.TableStorageClient tableClient = new TableStorageClient.TableStorageClient(StorageConnectionString);
+
+        // GET api/<controller>
+        public ActionResult GetAction(string eventId, string timeStamp)
+        {
+            tableClient.GetActionRecordsByPartitionAndSubkey("Action", eventId, timeStamp);
+
+            return Content("".ToString(), "application/json"); ;
+        }
+
+        // GET api/<controller>/5
+        public ActionResult GetEvent(string id, string name)
+        {
+            tableClient.GetEventRecordsByPartitionAndSubkey("Event", name, id);
+            return Content("".ToString(), "application/json"); ;
+        }
+
+        public ActionResult ExportAction(List<Action> actions)
+        {
+            //get path
+            var filePath = ExportAsCSV.Export<Action>(actions);
+            //convert to bytes
+            var fileData = System.IO.File.ReadAllBytes(filePath);
+            string contentType = MimeMapping.GetMimeMapping(filePath);
+            string[] directories = filePath.Split(Path.DirectorySeparatorChar);
+            string filename = directories[directories.Length - 1];
+            var cd = new System.Net.Mime.ContentDisposition
+            {
+                FileName = filename,
+                Inline = true,
+            };
+
+            Response.AppendHeader("Content-Disposition", cd.ToString());
+
+            return File(fileData, contentType);
+        }
+
+        public ActionResult ExportEvent(List<EventTable> events)
+        {
+            //get path
+            var filePath = ExportAsCSV.Export<EventTable>(events);
+            //convert to bytes
+            var fileData = System.IO.File.ReadAllBytes(filePath);
+            string contentType = MimeMapping.GetMimeMapping(filePath);
+            string filename = "";
+            var cd = new System.Net.Mime.ContentDisposition
+            {
+                FileName = filename,
+                Inline = true,
+            };
+
+            Response.AppendHeader("Content-Disposition", cd.ToString());
+
+            return File(fileData, contentType);
+        }
+    }
+}
---
+++ ReportingWeb/ReportingWeb/ExportFiles/ExportAsCSV.cs
@@ -0,0 +1,30 @@
+﻿using ReportingWeb.ExportFiles;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web;
+
+namespace ReportingWeb.ExportFiles
+{
+    //use TableStorageClient to get data and export it as csv
+    public static class ExportAsCSV
+    {
+        //return csv file according to Event id
+        //1. creae file.
+        //2. query data using client class
+        //3. store data and reutrn FileDetail
+
+        public static string Export<T>(List<T> input) where T : class
+        {
+            string path = @"C:\WINDOWS\TEMP\{0}_{1}.csv";
+            path = string.Format(path, DateTime.Now.Ticks, typeof(T).Name);
+            CsvExport<T> csv = new CsvExport<T>(input);
+            csv.ExportToFile(path);
+            return path;
+        }
+    }
+}
